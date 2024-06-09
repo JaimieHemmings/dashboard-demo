b49ed46a61f02acd41a0659e48c725b1df9cd6a2
Jaimie <jaimie.j.hemmings@gmail.com>
2024-06-09 18:00:36 +0100

    Added Theme Toggler

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,31 +1,31 @@
 import DashboardCard from '@/components/dashboard/DashboardCard';
-import { Folder, MessageCircle, Newspaper, Users } from 'lucide-react';
 import PostsTable from '@/components/posts/PostsTable';
-import AnalyticsChart from '@/components/dashboard/analyticsChart';
+import AnalyticsChart from '@/components/dashboard/AnalyticsChart';
+import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
         <DashboardCard
-          title="posts"
+          title="Posts"
           count={100}
-          icon={<Newspaper className="text-slate-500" size="72" />}
+          icon={<Newspaper className="text-slate-500" size={72} />}
         />
         <DashboardCard
           title="Categories"
           count={12}
-          icon={<Folder className="text-slate-500" size="72" />}
+          icon={<Folder className="text-slate-500" size={72} />}
         />
         <DashboardCard
           title="Users"
           count={750}
-          icon={<Users className="text-slate-500" size="72" />}
+          icon={<User className="text-slate-500" size={72} />}
         />
         <DashboardCard
           title="Comments"
           count={1200}
-          icon={<MessageCircle className="text-slate-500" size="72" />}
+          icon={<MessageCircle className="text-slate-500" size={72} />}
         />
       </div>
       <AnalyticsChart />
