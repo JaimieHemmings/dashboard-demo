fb0264720cb866d88a1b2c31288036a0ed08b880
Jaimie <jaimie.j.hemmings@gmail.com>
2024-06-09 18:16:22 +0100

    Added Log in page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import DashboardCard from '@/components/dashboard/DashboardCard';
 import PostsTable from '@/components/posts/PostsTable';
-import AnalyticsChart from '@/components/dashboard/AnalyticsChart';
+import AnalyticsChart from '@/components/dashboard/analyticsChart';
 import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 
 export default function Home() {
